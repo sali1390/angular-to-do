5673b1d1204e88cb4c8e0ebde049f9c9a4778f99
Saleema Ali <sali1390@gmail.com>
2017-03-09 00:20:18 -0500

    List To-Dos is now properly displaying data.

--- api/get-todos.js
+++ api/get-todos.js
@@ -2,12 +2,12 @@ var ToDo = require('../models/todos.js');
 
 module.exports = function(app) {
     app.get('/api/todos', function(req,res) {
-    ToDo.find({}, function(err, data) {
-        if(err) {
-            res.send(err);
-        } else {
-            res.json(data);
-        }
+        ToDo.find({}, function(err, data) {
+            if(err) {
+                res.send(err);
+            } else {
+                res.json(data);
+            }
+        })
     })
-})
-}
+};
